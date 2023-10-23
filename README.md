ef49b29b2a09dee12ddb87e0151aa9c7a6ff814d
fabrizia2 <melvinrenish56@gmail.com>
2023-10-23 23:36:45 +0300

    Healthy meals

---
+++ README.md
@@ -0,0 +1,8 @@
+This project demands a foundational
+understanding of HTML and CSS. You will learn
+how to add columns, divide sections, arrange
+items, add headers, footers. Most importantly,
+you will use your creativity to make the page
+look impressive. The alignments, the padding,
+the color palette, boxes and all the other
+elements on the page require attention.
---
+++ script.js
@@ -0,0 +1,90 @@
+// Sample data for menu items
+const menuItems = [
+    {
+        name: "Spaghetti Bolognese",
+        description: "Classic Italian pasta with rich meat sauce.",
+        image: "./images/elipse-3.svg",
+        price: "$12.99",
+    },
+    {
+        name: "Margarita Pizza",
+        description: "Simple and delicious pizza with tomato and mozzarella.",
+        image: "./images/elipse-4.svg",
+        price: "$12.99",
+    },
+    {
+        name: "Fresh Fruit Salad",
+        description: "A healthy mix of seasonal fruits.",
+        image: "./images/elipse-5.svg",
+        price: "$12.99",
+    },
+    {
+        name: "Iced Coffee",
+        description: "Chilled coffee with ice cubes and cream.",
+        image: "./images/elipse-6.svg",
+        price: "$12.99",
+    },
+];
+
+// Function to add menu items to the menu section
+function addMenuItemsToPage(menuItems) {
+    const menuItemsContainer = document.querySelector(".menu-items");
+
+    menuItems.forEach((item) => {
+        const menuItem = document.createElement("div");
+        menuItem.classList.add("menu-item");
+
+        const itemImage = document.createElement("img");
+        itemImage.src = item.image;
+        itemImage.alt = item.name;
+        itemImage.classList.add("item-image");
+
+        const itemDetails = document.createElement("div");
+        itemDetails.classList.add("item-details");
+
+        const itemName = document.createElement("h3");
+        itemName.textContent = item.name;
+        itemName.classList.add("item-name");
+
+        const itemPrice = document.createElement("p");
+        itemPrice.classList.add("price");
+        itemPrice.textContent = item.price;
+
+        const itemDescription = document.createElement("p");
+        itemDescription.textContent = item.description;
+
+        itemDetails.appendChild(itemName);
+        itemDetails.appendChild(itemPrice);
+        itemDetails.appendChild(itemDescription);
+
+        menuItem.appendChild(itemImage);
+        menuItem.appendChild(itemDetails);
+
+        menuItemsContainer.appendChild(menuItem);
+    });
+}
+
+
+// Add the menu items to the page
+addMenuItemsToPage(menuItems);
+
+// Open the menu modal when the "View Menu" button is clicked
+const viewMenuButton = document.getElementById('viewMenuButton');
+const menuModal = document.getElementById('menuModal');
+const closeModal = document.getElementById('closeModal');
+
+viewMenuButton.addEventListener('click', function() {
+    menuModal.style.display = 'block';
+});
+
+// Close the menu modal when the close button is clicked
+closeModal.addEventListener('click', function() {
+    menuModal.style.display = 'none';
+});
+
+// Close the menu modal when the user clicks outside of it
+window.addEventListener('click', function(event) {
+    if (event.target === menuModal) {
+        menuModal.style.display = 'none';
+    }
+});
